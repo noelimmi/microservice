9cae41df3d23e4467da9d6a734ff5d54538be660
Immanuel Irwin <immanuel.n@zohocorp.com>
2020-09-26 00:01:00 +0530

    Initial post GET and POST done

---
+++ MicroServicefromscratch/posts/index.js
@@ -0,0 +1,32 @@
+const express = require("express");
+const cors = require('cors');
+
+const { randomBytes } = require("crypto");
+
+const app = express();
+
+app.use(express.json());
+app.use(cors());
+
+const posts = {};
+
+app.get("/posts",(req,res)=>{
+  res.send(posts);
+});
+
+app.post("/posts",(req,res)=>{
+  const id = randomBytes(4).toString("hex");
+  const { title } = req.body;
+
+  posts[id] = {
+    id,title
+  }
+
+  res.status(201).send(posts[id]);
+});
+
+const PORT = 4000;
+
+app.listen(PORT,()=>{
+  console.log(`Server up and running in port ${PORT}`)
+});
